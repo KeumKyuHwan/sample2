977a737de0e883f30ddae17aa3354d1106cf2489
posilposil <khkum93@naver.com>
2024-11-12 10:34:06 +0900

    수정했다리

--- src/main/java/com/kosmo/ex/Main.java
+++ src/main/java/com/kosmo/ex/Main.java
@@ -2,7 +2,11 @@ package com.kosmo.ex;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("포실포실");
-        System.out.println("오네가이시마스");
+        System.out.println("실포실포");
+        System.out.println("포실실포포포포");
+        System.out.println("아리가또 오네가이시마스");
     }
+
 }
