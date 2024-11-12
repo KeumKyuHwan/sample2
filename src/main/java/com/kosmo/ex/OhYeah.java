4816fe9863c0be2c004151b600098e89a02387bd
posilposil <khkum93@naver.com>
2024-11-12 10:56:21 +0900

    새로운 프로젝트

---
+++ src/main/java/com/kosmo/ex/OhYeah.java
@@ -0,0 +1,8 @@
+package com.kosmo.ex;
+
+public class OhYeah {
+    public static void main(String[] args) {
+        System.out.println("포롱포롱포롱포롱포로로!");
+        System.out.println("노는게 제일 좋아");
+    }
+}
